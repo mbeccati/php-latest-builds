586fac33ede68e17911065d22dccc3a6d94b8f72
Matteo Beccati <matteo@beccati.com>
2024-01-11 11:12:01 +0100

    first commit

---
+++ configure.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ -f Makefile ]; then
+  make distclean
+fi
+
+./buildconf --force
+
+./configure \
+        --prefix=$PHP_DIR \
+        $PHP_CONF
+
+exit 0
+
+        --enable-debug \
+        --without-pear \
+        --with-mysqli=mysqlnd \
+        --with-pgsql=/usr \
+        --enable-pdo \
+        --with-pdo-sqlite \
+        --with-pdo-mysql=mysqlnd \
+        --with-pdo-pgsql=/usr \
+        --with-zlib \
+        --with-iconv \
+        --enable-bcmath \
+        --enable-ftp \
+        --enable-mbstring \
+        --with-curl \
+        --with-gettext \
+        --enable-intl \
+        --enable-sockets \
+        --enable-shmop \
+        --enable-sysvmsg \
+        --enable-sysvsem \
+        --enable-sysvshm \
+        --enable-soap \
+        --enable-fpm \
+        --enable-pcntl
