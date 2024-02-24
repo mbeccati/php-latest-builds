0e43d7367e4b5467b1640bcab880308112fc8aa1
Matteo Beccati <matteo@beccati.com>
2024-02-24 09:49:59 +0100

    Update php-composer.sh

--- php-composer.sh
+++ php-composer.sh
@@ -6,8 +6,19 @@ if [ -z "$PHP" ]; then
 fi
 
 sudo chmod +x $PHP
+
+EXPECTED_CHECKSUM="$($PHP -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 $PHP -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-$PHP -r "if (hash_file('sha384', 'composer-setup.php') === 'edb40769019ccf227279e3bdd1f5b2e9950eb000c3233ee85148944e555d97be3ea4f40c3c2fe73b22f875385f6a5155') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-$PHP composer-setup.php
-$PHP -r "unlink('composer-setup.php');"1
+ACTUAL_CHECKSUM="$($PHP -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+$PHP composer-setup.php --quiet
+rm composer-setup.php
+
 sudo mv composer.phar /usr/local/bin/composer
