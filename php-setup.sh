4032c2dd51c7214d0411831c7c11a3f96b50595a
Matteo Beccati <matteo@beccati.com>
2025-05-19 09:48:58 +0200

    Use pie

--- php-setup.sh
+++ php-setup.sh
@@ -5,8 +5,11 @@ if [[ -z "$PHP" || -z "$PHP_DIR" ]]; then
   exit 1
 fi
 
+# Setup PHP executables
 sudo chmod +x $PHP_DIR/bin/*
 
+
+# Composer
 EXPECTED_CHECKSUM="$($PHP -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 $PHP -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$($PHP -r "echo hash_file('sha384', 'composer-setup.php');")"
@@ -22,3 +25,8 @@ $PHP composer-setup.php --quiet
 rm composer-setup.php
 
 sudo mv composer.phar /usr/local/bin/composer
+
+
+# PIE
+sudo curl -sL --output /usr/local/bin/pie https://github.com/php/pie/releases/latest/download/pie.phar
+sudo chmod +x /usr/local/bin/pie
