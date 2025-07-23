55094f8b9e157616ac3344414fe17c908aa2dc94
Matteo Beccati <matteo@beccati.com>
2025-07-23 10:55:24 +0200

    Properly use GITHUB_TOKEN

--- php-setup.sh
+++ php-setup.sh
@@ -14,8 +14,7 @@ EXPECTED_CHECKSUM="$($PHP -r 'copy("https://composer.github.io/installer.sig", "
 $PHP -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$($PHP -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
+if [[ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]]; then
     >&2 echo 'ERROR: Invalid installer checksum'
     rm composer-setup.php
     exit 1
@@ -26,6 +25,9 @@ rm composer-setup.php
 
 sudo mv composer.phar /usr/local/bin/composer
 
+if [[ "$GITHUB_TOKEN" != "" ]]; then
+    $PHP /usr/local/bin/composer config --global --auth github-oauth.github.com "$GITHUB_TOKEN"
+fi
 
 # PIE
 sudo curl -sL --output /usr/local/bin/pie https://github.com/php/pie/releases/latest/download/pie.phar
